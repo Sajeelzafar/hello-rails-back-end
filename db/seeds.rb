e048635bc5be2b399b79bc20e522cd85c3721cda
Sajeel Zafar <sajeelzafar1995@gmail.com>
2023-03-09 08:00:55 +0500

    Populating database from seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greeting1 = Message.create!(greeting: "Hello")
+greeting2 = Message.create!(greeting: "Aloha")
+greeting3 = Message.create!(greeting: "Hola")
+greeting4 = Message.create!(greeting: "Bonjour")
+greeting5 = Message.create!(greeting: "Ciao")
